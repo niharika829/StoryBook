c5bbb19fc2d6cf23bf001650f4c6423d95c66f56
niharika829 <ns1324y@gmail.com>
2020-09-29 15:32:04 +0530

    morgan setup

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv'); //it will have all the config variables
 const connectDB = require('./config/db');
+const morgan = require('morgan');
+// morgan is used for logging.. so in the prject at any time if any request will be made (even between different webpages) ,it will be displayed on the console
 
 //to load config
 dotenv.config({ path: './config/config.env' });
@@ -10,4 +12,9 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+	//use midddleware
+	app.use(morgan('dev'));
+}
 app.listen(PORT, console.log(`server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
