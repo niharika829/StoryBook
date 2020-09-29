2a7974c1849d3348d3f3190fd8fa0a9a1b24df11
niharika829 <ns1324y@gmail.com>
2020-09-29 15:13:31 +0530

    created and exported mongoose module

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require(mongoose);
+
+//working with promises
+const connectDB = async () => {
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useFindAndModify: false,
+		});
+		console.log(`mongoose connected : ${conn.connection.host}`);
+	} catch (err) {
+		console.log(err);
+		process.exit(1);
+		//1 represent failure
+	}
+};
+module.exports = connectDB;
