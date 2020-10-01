d0029a7deea1a8eecfa9a2dfa929ec4ce3b3533b
niharika829 <ns1324y@gmail.com>
2020-10-01 08:19:42 +0530

    changed the default layout for login page

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,10 @@ const router = express.Router();
 //@method :- GET
 
 router.get('/', (req, res) => {
-	res.render('login');
+	res.render('login', {
+		layout: 'login',
+		// we personally changed the layout for login page , now this view is no longer having layout/main.hbs as its default layout
+	});
 });
 
 //@description:- dashboard landing page
