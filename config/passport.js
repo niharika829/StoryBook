be4219af71b29516f56bf62144de577fd313bfbd
niharika829 <ns1324y@gmail.com>
2020-10-03 16:17:00 +0530

    created userschema and googlestrategy

--- config/passport.js
+++ config/passport.js
@@ -1,2 +1,18 @@
-const googleStrategy = require('passport-google-oauth20');
+const GoogleStrategy = require('passport-google-oauth20');
 const mongoose = require('mongoose');
+const User = require('../models/User');
+
+module.exports = function (passport) {
+	passport.use(
+		new GoogleStrategy(
+			{
+				clientID: process.env.GOOGLE_CLIENT_ID,
+				clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+				callbackURL: 'auth/google/callback',
+			},
+			async (accessToken, refreshToken, profile, done) => {
+				console.log(profile);
+			}
+		)
+	);
+};
