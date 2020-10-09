e032fdd460f1bf24dcb30da00314eb627b00b55f
niharika829 <ns1324y@gmail.com>
2020-10-09 09:52:13 +0530

    added middleware to ensure protected routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
+const { ensureAuth, ensureGuest } = require('../middleware/auth');
 //@description:- login landing page
 //@rooute :-  /
 //@method :- GET
 
-router.get('/', (req, res) => {
+router.get('/', ensureGuest, (req, res) => {
 	res.render('login', {
 		layout: 'login',
 		// we personally changed the layout for login page , now this view is no longer having layout/main.hbs as its default layout
@@ -16,7 +16,8 @@ router.get('/', (req, res) => {
 //@rooute :-  /dashboard
 //@method :- GET
 
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', ensureAuth, (req, res) => {
+	console.log(req.user);
 	res.render('dashboard');
 });
 
