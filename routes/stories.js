f5a351f3435c2cd92efd994b8f4ac70a82d3e9c0
niharika829 <ns1324y@gmail.com>
2020-10-14 17:17:18 +0530

    added ckeditor to write stories

--- routes/stories.js
+++ routes/stories.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 const { ensureAuth } = require('../middleware/auth');
-const Story = require('../models/Story')
+const Story = require('../models/Story');
 //@desc    add stories
 //@route   GET /stories/add
-router.get('/add',ensureAuth, (req,res)  => {
-    res.render('/stories/add')
-})
+router.get('/add', ensureAuth, (req, res) => {
+	res.render('stories/add');
+});
 
-module.exports = router
+module.exports = router;
