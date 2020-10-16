ddb02ee5370fcd19ebe6de28bb86cd2d7401feb9
niharika829 <ns1324y@gmail.com>
2020-10-16 17:23:02 +0530

    created route for public stories

--- routes/stories.js
+++ routes/stories.js
@@ -19,4 +19,18 @@ router.post('/', ensureAuth, async (req, res) => {
 		res.render('/error/500');
 	}
 });
+
+//@desc    show all stories
+//@route   GET /stories
+router.get('/', ensureAuth, async (req, res) => {
+	try {
+		const stories = await Story.find({ status: 'public' }).populate('user').sort({ createdAt: 'desc' }).lean();
+		res.render('stories/index', {
+			stories,
+		});
+	} catch (err) {
+		console.log(err);
+		res.render('/error/500');
+	}
+});
 module.exports = router;
