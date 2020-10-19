cb379dcfba40224e04b4d0944ffe5d56cc37cb78
niharika829 <ns1324y@gmail.com>
2020-10-19 07:55:26 +0530

    created route for edit page

--- routes/stories.js
+++ routes/stories.js
@@ -33,4 +33,20 @@ router.get('/', ensureAuth, async (req, res) => {
 		res.render('/error/500');
 	}
 });
+
+//@desc    stories edit page
+//@route   GET /stories/edit/:id
+router.get('/edit/:id', ensureAuth, async (req, res) => {
+	const story = Story.findOne({ _id: req.params.id }).lean();
+	if (!story) {
+		return res.render('error/404');
+	}
+	if (story.user != req.user.id) {
+		res.redirect('/stories');
+	} else {
+		res.render('/stories/edit', {
+			story,
+		});
+	}
+});
 module.exports = router;
